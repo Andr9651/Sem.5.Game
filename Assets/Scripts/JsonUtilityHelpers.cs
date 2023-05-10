15ad9ab84270282b21dc3dd49a7ca283305d8ceb
Andreas Clement Holmgaard <Holmi1@live.dk>
2023-05-10 16:40:26 +0200

    Add JsonUtility helper

---
+++ Assets/Scripts/JsonUtilityHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public static class JsonUtilityHelpers
+{
+	public static T[] FromJsonArray<T>(string json)
+	{
+		string wrappedJson = "{\"Array\":" + json + "}";
+		ArrayWrapper<T> arrayWrapper = JsonUtility.FromJson<ArrayWrapper<T>>(wrappedJson);
+		return arrayWrapper.Array;
+	}
+
+	[Serializable]
+	internal class ArrayWrapper<T>
+	{
+		public T[] Array;
+	}
+}
