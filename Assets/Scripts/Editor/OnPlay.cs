5956d396fb7b5abe02ffd8a589e247ad9aac433f
Andreas Clement Holmgaard <Holmi1@live.dk>
2023-05-12 09:34:11 +0200

    Add editor script to load UI scene on play

---
+++ Assets/Scripts/Editor/OnPlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Editor
+{
+	[InitializeOnLoad]
+	public class OnPlay
+	{
+		static OnPlay()
+		{
+			EditorApplication.playModeStateChanged += LoadUIOnTrackScene;
+		}
+
+		static void LoadUIOnTrackScene(PlayModeStateChange stateChange)
+		{
+			if (stateChange == PlayModeStateChange.EnteredPlayMode)
+			{
+				string currentSceneName = SceneManager.GetActiveScene().name;
+				if (currentSceneName.Contains("Track"))
+				{
+					// Loads the scene with index 0 as seen in "File/Build Settings..."
+					SceneManager.LoadScene(0, LoadSceneMode.Additive);
+				}
+			}
+			
+		}
+	}
+}
