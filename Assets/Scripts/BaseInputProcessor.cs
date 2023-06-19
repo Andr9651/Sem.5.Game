ec3d0afb3c4a3c48cfadfc1ec9f61539c48f1cf8
Andreas Clement Holmgaard <Holmi1@live.dk>
2023-06-19 11:02:05 +0200

    Input processors and tire heating

---
+++ Assets/Scripts/BaseInputProcessor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInputProcessor : ScriptableObject
+{
+	protected float _leftWheelInput;
+	protected float _rightWheelInput;
+
+	public float LeftWheelInput
+	{
+		get => ProcessInput(_leftWheelInput);
+		set => _leftWheelInput = value;
+	}
+
+	public float RightWheelInput
+	{
+		get => ProcessInput(_rightWheelInput);
+		set => _rightWheelInput = value;
+	}
+
+
+	protected virtual float ProcessInput(float input)
+	{
+		return input;
+	}
+
+}
